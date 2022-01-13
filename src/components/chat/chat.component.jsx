363ffd7e7afab66277c93e03c31145bb85765cfa
mtaba <tabatabaee.mostafa@gmail.com>
2022-01-13 07:45:27 +0330

    add redirect to chat component

--- src/components/chat/chat.component.jsx
+++ src/components/chat/chat.component.jsx
@@ -1,19 +1,24 @@
 import {connect} from 'react-redux';
+import {Redirect, useHistory} from 'react-router-dom'
 import Avatar from "../avatar/avatar.component";
 import UserCaption from "../userCaption/userCaption.component";
 import  setCurrentChat from '../../redux/chat/chat.actions';
 
 import './chat.styles.scss';
 
-function Chat({setCurrentChat, chat}){
-
+function Chat(props){
+    const history = useHistory();
+//    const {setCurrentChat, chat} = props;
     function handleClick(){
-        setCurrentChat(chat);
+        setCurrentChat(props.chat);
+        history.push("/chat");
+        // props.history.push("/chat");
+       
     }
   return  (
           <li  className="chatlist-chat" onClick={handleClick}>
           <Avatar />
-          <UserCaption  messages={chat.messages} />
+          <UserCaption  messages={props.chat.messages} />
           </li>
   ) 
 }
